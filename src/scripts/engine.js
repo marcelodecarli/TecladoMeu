c5bd2296913ad656802ae70a108dc6f2196fd7a2
Marcelo <sl_marcelo@hotmail.com>
2024-11-25 12:14:12 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,44 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key")
+let audio = new Audio()
+
+let mapedKeys = [];
+const volume = document.querySelector(".volume input")
+
+const keysCheck = document.querySelector(".kyes-check input")
+
+
+const playTune = (key) => {
+  audio.src = `src/tunes/${key}.wav`
+  audio.play()
+
+  const clickedKey = document.querySelector(`[data-key="${key}"]`)
+
+  clickedKey.classList.add("active")
+  setTimeout(() => {
+    clickedKey.classList.remove("active")
+  }, 300)
+
+}
+
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => playTune(key.dataset.key))
+  mapedKeys.push(key.dataset.key)
+})
+
+document.addEventListener('keydown', (e) => {
+  if (mapedKeys.includes(e.key)) { playTune(e.key) }
+
+})
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value
+
+}
+
+volume.addEventListener("input", handleVolume)
+
+const showHideKeys = () => {
+  pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+keysCheck.addEventListener("click", showHideKeys)
